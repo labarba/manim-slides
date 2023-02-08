d1ffae37c4bb8358f4671d51eaf1bd28a2082709
Jérome Eertmans <jeertmans@icloud.com>
2023-02-08 18:03:08 +0100

    wip: ipython magic

--- manim_slides/__init__.py
+++ manim_slides/__init__.py
@@ -1,3 +1,17 @@
 # flake8: noqa: F401
 from .__version__ import __version__
 from .slide import Slide, ThreeDSlide
+from .manim import MANIM
+
+
+if MANIM:
+    try:
+        from IPython import get_ipython
+    
+        from .ipython_magic import ManimSlidesMagic
+    except ImportError:
+        pass
+    else:
+        ipy = get_ipython()
+        if ipy is not None:
+            ipy.register_magics(ManimSlidesMagic)
